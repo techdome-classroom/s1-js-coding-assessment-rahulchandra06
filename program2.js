764f285ea15e2297bdde94f20c82b0bca08d02fa
Rahul Chandrawanshi <141754004+rahulchandra06@users.noreply.github.com>
2024-10-07 12:42:42 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,38 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  const memo = {};
 
-  };
+  function isMatch(i, j) {
+   
+    if (`${i},${j}` in memo) {
+      return memo[`${i},${j}`];
+    }
+
+
+    if (i === s.length && j === p.length) return true;
+
+    if (j === p.length) return false;
+
+
+    const currentMatch = i < s.length && (s[i] === p[j] || p[j] === '?');
+
+    if (p[j] === '*') {
+
+      memo[`${i},${j}`] = (isMatch(i, j + 1) || (i < s.length && isMatch(i + 1, j)));
+      return memo[`${i},${j}`];
+    }
+
   
-  module.exports = decodeTheRing;
+    if (currentMatch) {
+      memo[`${i},${j}`] = isMatch(i + 1, j + 1);
+      return memo[`${i},${j}`];
+    }
+
+    memo[`${i},${j}`] = false;
+    return false;
+  }
+
+  return isMatch(0, 0);
+};
+
+module.exports = decodeTheRing;
